a8041ed1299a640348b1a7d5cdef33937c35e9be
shipperizer <alexcabb@gmail.com>
2022-01-05 16:42:38 +0000

    fix: drop sigkill catching

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -103,7 +103,7 @@ func main() {
 	}()
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGKILL)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
 	// Block until we receive our signal.
 	<-c
--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -79,7 +79,7 @@ func main() {
 	}()
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGKILL)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
 	// Block until we receive our signal.
 	<-c
